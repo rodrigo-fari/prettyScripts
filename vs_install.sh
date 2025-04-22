88347eaa0360a146390455e5192f734db783d3ae
Rodrigo Falcao <rodrigofal44@gmail.com>
2025-04-22 23:51:13 +0100

    finish final script

--- vs_install.sh
+++ vs_install.sh
@@ -2,9 +2,8 @@
 
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin dest=$HOME/kitty
 
-# Criar alias para o Kitty no .zshrc
 if ! grep -q "alias kitty=" ~/.zshrc; then
-    echo 'alias kitty="setsid $HOME/kitty/kitty.app/bin/kitty >/dev/null 2>&1 < /dev/null &"' >> ~/.zshrc
+	echo 'alias kitty="setsid $HOME/kitty/kitty.app/bin/kitty >/dev/null 2>&1 < /dev/null &"' >> ~/.zshrc
 fi
 
 source ~/.zshrc
